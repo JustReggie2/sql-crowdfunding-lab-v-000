fd36d0c0e73c2634a741ea788c39c0319be274a3
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-09-15 20:50:00 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -3,8 +3,8 @@ CREATE TABLE projects (
   title TEXT,
   category TEXT,
   funding_goal INTEGER,
-  start_date DATE,
-  end_date DATE
+  start_date INTEGER,
+  end_date INTEGER
 );
 
 CREATE TABLE users (
